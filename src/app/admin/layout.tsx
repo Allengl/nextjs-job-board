898419042c96212d385c5c1c3f9c9849a52d671c
root <root@Allengl>
2024-02-19 12:07:54 +0800

    feat: add admin page

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,11 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metaData: Metadata = {
+  title: "Admin",
+};
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <ClerkProvider>{children}</ClerkProvider>;
+};
+
+export default Layout;
